e3e62f8fd0b6afad2eda12e6ee5f71149a2efe29
do0dleman <97869897+do0dleman@users.noreply.github.com>
2023-12-26 12:11:13 +0200

    changed Title1 size on mobiles

--- src/app/ui/Titles/Title1.tsx
+++ src/app/ui/Titles/Title1.tsx
@@ -3,7 +3,7 @@ export default function Title1(props: React.DetailedHTMLProps<React.HTMLAttribut
     const { className, children, ...rest } = props
 
     return (
-        <h1 className={`text-6xl py-12 text-center md:text-left leading-snug ${className}`} {...rest}>
+        <h1 className={`text-4xl md:text-6xl py-12 text-center md:text-left leading-snug ${className}`} {...rest}>
             {children}
         </h1>
     )
